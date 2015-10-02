3b2b59056c67a62861f2114c6caa34c7e5b1e9e6
andyprastyo <andy.greco@gmail.com>
2015-10-02 16:42:45 +0700

    Tugas 1
    Pengumpulan Tugas pertama

---
+++ TI-2E-1441180119/TI-2E-1441180119/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Blue Then
+            Me.BackColor = Color.Yellow
+        ElseIf Me.BackColor = Color.Yellow Then
+            Me.BackColor = Color.Blue
+        End If
+    End Sub
+End Class
